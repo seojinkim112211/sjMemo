4d915e8c7aef1929eb76f2c73727a99902bf9072
kimseojin <zzin2990@gmail.com>
2023-08-21 09:20:07 +0900

    Initial Commit

---
+++ SjMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SjMemo
+//
+//  Created by t2023-m0087 on 2023/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
